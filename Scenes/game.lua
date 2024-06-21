021dd9c227486e48a4f4634a8ee836cfbc8d5eb1
Logan Peterson <Brushfire257@gmail.com>
2024-06-21 18:01:07 -0400

    Working on collisions
    Collisions not working yet
    (Added a debug mode)

--- Scenes/game.lua
+++ Scenes/game.lua
@@ -1,8 +1,5 @@
 -- The arcade loop for the game.
 
--- To Do List:
--- Make a to do list
-
 -- Scene container
 arcadeGame = {}
 
@@ -12,6 +9,9 @@ local screenHeight = love.graphics.getHeight()
 local timer = 0
 
 local backgroundSpeed = 0
+
+-- Library(s)
+HC = require 'HardonCollider'
 
 function arcadeGame.load() -- Runs once at the start of the game.
     -- Load window values
@@ -25,6 +25,10 @@ function arcadeGame.load() -- Runs once at the start of the game.
     -- Load the player car
     loadCar()
     loadRoad()
+
+    -- Load Game settings (currently just debug mode)
+    local settingsStr = love.filesystem.read("settings.txt")
+    loadSettings(settingsStr)
 end
 
 function arcadeGame.update(dt) -- Runs every frame.
@@ -36,12 +40,30 @@ end
 function arcadeGame.draw() -- Draws every frame / Runs directly after love.update()
     love.graphics.draw(road.image, road.x, road.v1y) -- Draws the road sprites
     love.graphics.draw(road.image, road.x, road.v2y)
-    love.graphics.draw(carSprite.image, carSprite.x, carSprite.y, carSprite.rotation, carSprite.scaleX, carSprite.scaleY) -- Draws the car sprite
+
+    love.graphics.draw(carSprite.image, carSprite.x, carSprite.y,
+    carSprite.rotation, carSprite.scaleX, carSprite.scaleY) -- Draws the car sprite
+
+    -- Draw the edges of car collider
+    if debugMode then
+        love.graphics.setColor(1, 0, 0) -- Set the color to red
+        points = {carCollider:points()}
+        for i = 1, #points, 2 do
+            local next_i = i + 2
+            if next_i > #points then next_i = 1 end
+            love.graphics.line(points[i], points[i+1], points[next_i], points[next_i+1])
+        end
+        love.graphics.setColor(1, 1, 1) -- Reset the color to white
+    end
 end
 
 function loadCar()
     carSprite = love.graphics.newImage("Sprites/yellowcar.png")
-    carSprite = loadObject(objectName, ((screenWidth/2)), 1000, (-math.pi/2), 1, 1, 30, "Sprites/yellowcar.png", 60, 30)
+    carSprite = loadObject("playerCar", ((screenWidth/2)), 1000, (-math.pi/2), 1, 1, 30, "Sprites/yellowcar.png", 60, 30)
+    
+    --polygon collider for the car
+    carCollider = HC.polygon(carSprite.x, carSprite.y, carSprite.x + carSprite.width, carSprite.y,
+    carSprite.x + carSprite.width, carSprite.y + carSprite.height, carSprite.x, carSprite.y + carSprite.height)
 end
 
 function loadObject(objectName, x, y, rotation, scaleX, scaleY, health, image, width, height)
@@ -72,6 +94,10 @@ function playerUpdate(dt)
     elseif love.keyboard.isDown('down') then
         backgroundSpeed = backgroundSpeed + carSprite.accel * dt
     end
+
+    -- Update collider position and rotation
+    carCollider:moveTo(carSprite.x, carSprite.y)
+    carCollider:rotate(carSprite.rotation - carCollider:rotation(), carCollider:center())
 end
 
 function loadRoad()
@@ -110,5 +136,16 @@ function roadUpdate(dt)
     end
 end
 
+function loadSettings(settingsStr)
+    for line in settingsStr:gmatch("[^\r\n]+") do
+        local key, value = line:match("(%w+)%s*=%s*(%w+)")
+        if key and value then
+            if key == "debugMode" then
+                debugMode = tonumber(value) == 1
+            end
+            -- Add more settings here...
+        end
+    end
+end
 
 return arcadeGame
