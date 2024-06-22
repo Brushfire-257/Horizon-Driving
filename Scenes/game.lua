4221df02654eaa6a49351a774d6f036c3673ab2b
Logan Peterson <Brushfire257@gmail.com>
2024-06-22 09:42:28 -0400

    Road Fixes and Camera Improvements
    Fixed road display bug and improved camera motion

--- Scenes/game.lua
+++ Scenes/game.lua
@@ -47,14 +47,11 @@ end
 
 function arcadeGame.draw() -- Draws every frame / Runs directly after love.update()
     camera:attach()
-    love.graphics.draw(road.image, road.x, road.v1y,
-    0, road.scaleX, road.scaleY) -- Draws the road sprites
-    love.graphics.draw(road.image, road.x, road.v2y,
-    0, road.scaleX, road.scaleY)
+    love.graphics.draw(road.image, road.x, road.v1y, 0, road.scaleX, road.scaleY) -- Draws the road sprites
+    love.graphics.draw(road.image, road.x, road.v2y, 0, road.scaleX, road.scaleY)
 
-    love.graphics.draw(carSprite.image, carSprite.x, carSprite.y,
-    carSprite.rotation, carSprite.scaleX, carSprite.scaleY,
-    carSprite.rotationX, carSprite.rotationY) -- Draws the car sprite
+    love.graphics.draw(carSprite.image, carSprite.x, carSprite.y, carSprite.rotation, carSprite.scaleX, carSprite.scaleY,
+        carSprite.rotationX, carSprite.rotationY) -- Draws the car sprite
 
     -- Draw the edges of car collider
     if debugMode then
@@ -76,10 +73,10 @@ function loadCar()
     scaleX = 0.5
     scaleY = 0.5
     image = love.graphics.newImage("Sprites/yellowcar.png")
-    carSprite = loadObject("playerCar", ((screenWidth/2)), 1000, (-math.pi/2), scaleX, scaleY, 30, "Sprites/yellowcar.png",
-    (image:getWidth()*scaleX), (image:getHeight()*scaleY), (image:getWidth()/2), (image:getHeight()/2))
-    
-    --polygon collider for the car
+    carSprite = loadObject("playerCar", ((screenWidth / 2)), 1000, (-math.pi / 2), scaleX, scaleY, 30, "Sprites/yellowcar.png",
+        (image:getWidth() * scaleX), (image:getHeight() * scaleY), (image:getWidth() / 2), (image:getHeight() / 2))
+
+    -- polygon collider for the car
     carCollider = HC.polygon(
         carSprite.x, carSprite.y,
         carSprite.x + carSprite.width, carSprite.y,
@@ -99,7 +96,7 @@ function loadObject(objectName, x, y, rotation, scaleX, scaleY, health, image, w
         scaleX = scaleX,
         scaleY = scaleY,
         speed = 0,
-        accel = 250,
+        accel = 300,
         rotationSpeed = 2,
         width = width,
         height = height,
@@ -116,8 +113,10 @@ function playerUpdate(dt)
     end
     if love.keyboard.isDown('up') then -- Moving
         carSprite.speed = carSprite.speed + carSprite.accel * dt
+        camerayShake = camerayShake - carSprite.accel / 5 * carSprite.speed / 1000
     elseif love.keyboard.isDown('down') then
         carSprite.speed = carSprite.speed - carSprite.accel * dt
+        camerayShake = camerayShake + carSprite.accel / 5 * carSprite.speed / 1000
     end
 
     local dx = carSprite.speed * math.cos(carSprite.rotation)
@@ -134,10 +133,15 @@ function playerUpdate(dt)
 end
 
 function cameraLERP(dt)
+    if camerayShake > 100 then
+        camerayShake = 100
+    elseif camerayShake < -100 then
+        camerayShake = -100
+    end
     -- Calculate the distance to the player
     local dx = carSprite.x - camera.x
     local dy = carSprite.y - camera.y - camerayOffset - camerayShake
-    camerayShake = camerayShake*0.9
+    camerayShake = camerayShake * 0.9
 
     local lerpFactor = 1.5 -- camera speed
     camera:move(dx * lerpFactor * dt, dy * lerpFactor * dt)
@@ -149,10 +153,9 @@ function loadRoad()
     
     road = {
         image = image, -- Art pass comes later ..
-        x = ((screenWidth/2)-(roadScale*image:getWidth()/2)),
-        v2x = 0,
+        x = ((screenWidth / 2) - (roadScale * image:getWidth() / 2)),
         v1y = 0,
-        v2y = image:getHeight(),
+        v2y = image:getHeight() * roadScale,
         scaleX = roadScale,
         scaleY = roadScale,
     }
@@ -161,16 +164,16 @@ function loadRoad()
     leftRoadColliderOffset = 1570
 
     rightRoadCollider = HC.polygon(
-        ((road.x + road.image:getWidth()/2) + rightRoadColliderOffset), 0,
-        ((road.x + road.image:getWidth()/2) + rightRoadColliderOffset) + 50, 0,
-        ((road.x + road.image:getWidth()/2) + rightRoadColliderOffset) + 50, 2000,
-        ((road.x + road.image:getWidth()/2) + rightRoadColliderOffset), 2000
+        ((road.x + road.image:getWidth() / 2) + rightRoadColliderOffset), 0,
+        ((road.x + road.image:getWidth() / 2) + rightRoadColliderOffset) + 50, 0,
+        ((road.x + road.image:getWidth() / 2) + rightRoadColliderOffset) + 50, 2000,
+        ((road.x + road.image:getWidth() / 2) + rightRoadColliderOffset), 2000
     )
     leftRoadCollider = HC.polygon(
-        ((road.x + road.image:getWidth()/2) - leftRoadColliderOffset), 0,
-        ((road.x + road.image:getWidth()/2) - leftRoadColliderOffset) - 50, 0,
-        ((road.x + road.image:getWidth()/2) - leftRoadColliderOffset) - 50, 2000,
-        ((road.x + road.image:getWidth()/2) - leftRoadColliderOffset), 2000
+        ((road.x + road.image:getWidth() / 2) - leftRoadColliderOffset), 0,
+        ((road.x + road.image:getWidth() / 2) - leftRoadColliderOffset) - 50, 0,
+        ((road.x + road.image:getWidth() / 2) - leftRoadColliderOffset) - 50, 2000,
+        ((road.x + road.image:getWidth() / 2) - leftRoadColliderOffset), 2000
     )
     table.insert(colliders, rightRoadCollider)
     table.insert(colliders, leftRoadCollider)
@@ -181,41 +184,41 @@ function roadUpdate(dt)
     road.v2y = road.v2y - roadFrameMove * dt
 
     if roadFrameMove > 0 then
-        if road.v1y < -road.image:getHeight()*roadScale then
-            road.v1y = road.v2y + road.image:getHeight()*roadScale
+        if road.v1y < -road.image:getHeight() * road.scaleY then
+            road.v1y = road.v2y + road.image:getHeight() * road.scaleY
         end
 
-        if road.v2y < -road.image:getHeight()*roadScale then
-            road.v2y = road.v1y + road.image:getHeight()*roadScale
+        if road.v2y < -road.image:getHeight() * road.scaleY then
+            road.v2y = road.v1y + road.image:getHeight() * road.scaleY
         end
 
     elseif roadFrameMove < 0 then
-        if road.v1y > road.image:getHeight()*roadScale then
-            road.v1y = road.v2y - road.image:getHeight()*roadScale
+        if road.v1y > road.image:getHeight() * road.scaleY then
+            road.v1y = road.v2y - road.image:getHeight() * road.scaleY
         end
 
-        if road.v2y > road.image:getHeight()*roadScale then
-            road.v2y = road.v1y - road.image:getHeight()*roadScale
+        if road.v2y > road.image:getHeight() * road.scaleY then
+            road.v2y = road.v1y - road.image:getHeight() * road.scaleY
         end
     end
     -- Update colliders
-    rightRoadCollider:moveTo(((road.x + road.image:getWidth()*roadScale/2) + rightRoadColliderOffset), 1000)
-    leftRoadCollider:moveTo(((road.x + road.image:getWidth()*roadScale/2) - leftRoadColliderOffset), 1000)
+    rightRoadCollider:moveTo(((road.x + road.image:getWidth() * road.scaleX / 2) + rightRoadColliderOffset), 1000)
+    leftRoadCollider:moveTo(((road.x + road.image:getWidth() * road.scaleX / 2) - leftRoadColliderOffset), 1000)
     table.insert(colliders, rightRoadCollider)
     table.insert(colliders, leftRoadCollider)
 
     -- Check collider collisions
     if carCollider:collidesWith(rightRoadCollider) then
-        carSprite.x = (road.x + road.image:getWidth()*roadScale/2) + rightRoadColliderOffset - (carSprite.image:getWidth()/3)
-        carSprite.rotation = -math.rad(90+15)
+        carSprite.x = (road.x + road.image:getWidth() * road.scaleX / 2) + rightRoadColliderOffset - (carSprite.image:getWidth() / 3)
+        carSprite.rotation = -math.rad(90 + 15)
         carSprite.speed = carSprite.speed - 250
         camerayShake = camerayShake + 200
         print(carSprite.speed)
     elseif carCollider:collidesWith(leftRoadCollider) then
-        carSprite.x = (road.x + road.image:getWidth()*roadScale/2) - leftRoadColliderOffset + (carSprite.image:getWidth()/3)
-        carSprite.rotation = -math.rad(90-15)
+        carSprite.x = (road.x + road.image:getWidth() * road.scaleX / 2) - leftRoadColliderOffset + (carSprite.image:getWidth() / 3)
+        carSprite.rotation = -math.rad(90 - 15)
         carSprite.speed = carSprite.speed - 250
-        camerayShake =  camerayShake + 200
+        camerayShake = camerayShake + 200
         print(carSprite.speed)
     end
     if carSprite.speed < 100 then
