fe9ee5d2160dfa43f537dc2992a296bf6387c583
Logan Peterson <Brushfire257@gmail.com>
2024-06-22 08:37:41 -0400

    CAMERA SYSTEM!!!!
    Better wider road as well

--- Scenes/game.lua
+++ Scenes/game.lua
@@ -11,8 +11,9 @@ local colliders = {}
 
 local backgroundSpeed = 0
 
--- Library(s)
+-- Libraries
 HC = require 'HardonCollider'
+local Camera = require 'hump.camera'
 
 function arcadeGame.load() -- Runs once at the start of the game.
     -- Load window values
@@ -27,6 +28,11 @@ function arcadeGame.load() -- Runs once at the start of the game.
     loadCar()
     loadRoad()
 
+    -- Create camera
+    camerayOffset = 400
+    camerayShake = 0
+    camera = Camera(carSprite.x, carSprite.y - camerayOffset)
+
     -- Load Game settings (currently just debug mode)
     local settingsStr = love.filesystem.read("settings.txt")
     loadSettings(settingsStr)
@@ -40,8 +46,11 @@ function arcadeGame.update(dt) -- Runs every frame.
 end
 
 function arcadeGame.draw() -- Draws every frame / Runs directly after love.update()
-    love.graphics.draw(road.image, road.x, road.v1y) -- Draws the road sprites
-    love.graphics.draw(road.image, road.x, road.v2y)
+    camera:attach()
+    love.graphics.draw(road.image, road.x, road.v1y,
+    0, road.scaleX, road.scaleY) -- Draws the road sprites
+    love.graphics.draw(road.image, road.x, road.v2y,
+    0, road.scaleX, road.scaleY)
 
     love.graphics.draw(carSprite.image, carSprite.x, carSprite.y,
     carSprite.rotation, carSprite.scaleX, carSprite.scaleY,
@@ -60,6 +69,7 @@ function arcadeGame.draw() -- Draws every frame / Runs directly after love.updat
         end
         love.graphics.setColor(1, 1, 1) -- Reset the color to white
     end
+    camera:detach()
 end
 
 function loadCar()
@@ -118,21 +128,37 @@ function playerUpdate(dt)
     carCollider:moveTo(carSprite.x, carSprite.y)
     carCollider:rotate(carSprite.rotation - carCollider:rotation(), carCollider:center())
     table.insert(colliders, carCollider)
+
+    -- Update player camera
+    cameraLERP(dt)
+end
+
+function cameraLERP(dt)
+    -- Calculate the distance to the player
+    local dx = carSprite.x - camera.x
+    local dy = carSprite.y - camera.y - camerayOffset - camerayShake
+    camerayShake = camerayShake*0.9
+
+    local lerpFactor = 1.5 -- camera speed
+    camera:move(dx * lerpFactor * dt, dy * lerpFactor * dt)
 end
 
 function loadRoad()
-    image = love.graphics.newImage("Sprites/road.png")
-
+    image = love.graphics.newImage("Sprites/road1.png")
+    roadScale = 4
+    
     road = {
         image = image, -- Art pass comes later ..
-        x = ((screenWidth/2)-(image:getWidth()/2)),
+        x = ((screenWidth/2)-(roadScale*image:getWidth()/2)),
         v2x = 0,
         v1y = 0,
         v2y = image:getHeight(),
+        scaleX = roadScale,
+        scaleY = roadScale,
     }
 
-    rightRoadColliderOffset = 350
-    leftRoadColliderOffset = 400
+    rightRoadColliderOffset = 1570
+    leftRoadColliderOffset = 1570
 
     rightRoadCollider = HC.polygon(
         ((road.x + road.image:getWidth()/2) + rightRoadColliderOffset), 0,
@@ -155,39 +181,41 @@ function roadUpdate(dt)
     road.v2y = road.v2y - roadFrameMove * dt
 
     if roadFrameMove > 0 then
-        if road.v1y < -road.image:getHeight() then
-            road.v1y = road.v2y + road.image:getHeight()
+        if road.v1y < -road.image:getHeight()*roadScale then
+            road.v1y = road.v2y + road.image:getHeight()*roadScale
         end
 
-        if road.v2y < -road.image:getHeight() then
-            road.v2y = road.v1y + road.image:getHeight()
+        if road.v2y < -road.image:getHeight()*roadScale then
+            road.v2y = road.v1y + road.image:getHeight()*roadScale
         end
 
     elseif roadFrameMove < 0 then
-        if road.v1y > road.image:getHeight() then
-            road.v1y = road.v2y - road.image:getHeight()
+        if road.v1y > road.image:getHeight()*roadScale then
+            road.v1y = road.v2y - road.image:getHeight()*roadScale
         end
 
-        if road.v2y > road.image:getHeight() then
-            road.v2y = road.v1y - road.image:getHeight()
+        if road.v2y > road.image:getHeight()*roadScale then
+            road.v2y = road.v1y - road.image:getHeight()*roadScale
         end
     end
     -- Update colliders
-    rightRoadCollider:moveTo(((road.x + road.image:getWidth()/2) + rightRoadColliderOffset), 1000)
-    leftRoadCollider:moveTo(((road.x + road.image:getWidth()/2) - leftRoadColliderOffset), 1000)
+    rightRoadCollider:moveTo(((road.x + road.image:getWidth()*roadScale/2) + rightRoadColliderOffset), 1000)
+    leftRoadCollider:moveTo(((road.x + road.image:getWidth()*roadScale/2) - leftRoadColliderOffset), 1000)
     table.insert(colliders, rightRoadCollider)
     table.insert(colliders, leftRoadCollider)
 
     -- Check collider collisions
     if carCollider:collidesWith(rightRoadCollider) then
-        carSprite.x = (road.x + road.image:getWidth()/2) + rightRoadColliderOffset - (carSprite.image:getWidth()/3)
-        carSprite.rotation = -2*math.pi/3
+        carSprite.x = (road.x + road.image:getWidth()*roadScale/2) + rightRoadColliderOffset - (carSprite.image:getWidth()/3)
+        carSprite.rotation = -math.rad(90+15)
         carSprite.speed = carSprite.speed - 250
+        camerayShake = camerayShake + 200
         print(carSprite.speed)
     elseif carCollider:collidesWith(leftRoadCollider) then
-        carSprite.x = (road.x + road.image:getWidth()/2) - leftRoadColliderOffset + (carSprite.image:getWidth()/3)
-        carSprite.rotation = -math.pi/3
+        carSprite.x = (road.x + road.image:getWidth()*roadScale/2) - leftRoadColliderOffset + (carSprite.image:getWidth()/3)
+        carSprite.rotation = -math.rad(90-15)
         carSprite.speed = carSprite.speed - 250
+        camerayShake =  camerayShake + 200
         print(carSprite.speed)
     end
     if carSprite.speed < 100 then
